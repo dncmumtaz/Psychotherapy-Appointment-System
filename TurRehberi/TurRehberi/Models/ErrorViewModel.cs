535005787fcb7f20a84f0e69d762b2a02115608f
TugayIpek <tgypk.gp@gmail.com>
2021-12-26 12:23:48 +0300

    Startup

---
+++ TurRehberi/TurRehberi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TurRehberi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
